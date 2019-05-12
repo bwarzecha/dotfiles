1ad871204b5308c2323f96d7f226420d42ad509b
bwarzecha <bartosz@warzecha.co.uk>
2019-05-12 17:14:43 +0100

    A bit better logger

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -21,18 +21,43 @@ spoon.MiroWindowsManager:bindHotkeys({ up = {hyper, "up"},
 
 hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'L', function() hs.caffeinate.startScreensaver() end)
 
+events_buffer = {}
+events_buffer.length = 0
+function log_event(name, details)
+    event = {
+        timestamp = os.date("%x %X", os.time()),
+        name = name,
+        details = details or {}
+    }
+    table.insert(events_buffer, event)
+    events_buffer.length = events_buffer.length + 1
+    print('buffer: '.. events_buffer.length)
+end
+
+
 -- turn off  bluetooth on sleen (lid closed)
 function bluetooth(power)
     print("Setting bluetooth to " .. power)
     hs.execute([["blueutil" "--power "  "power"]], true)
 end
 
-function f(event)
+function caffeineEvents(event)
 		print('Received event' ..  event)
     if event == hs.caffeinate.watcher.systemWillSleep then
+        log_event('system_will_sleep')
         bluetooth("off")
+        flush_events()
     elseif event == hs.caffeinate.watcher.screensDidWake then
+        log_event('screen_did_wake')
         bluetooth("on")
+    elseif  event ==  hs.caffeinate.watcher.screensDidUnlock then
+        log_event('screen_did_unlock')
+    elseif  event ==  hs.caffeinate.watcher.screensDidLock then
+        log_event('screen_did_lock')
+    elseif  event ==  hs.caffeinate.watcher.screensaverDidStart then
+        log_event('screensaver_did_start')
+    elseif  event ==  hs.caffeinate.watcher.screensaverDidStop then
+        log_event('screensaver_did_stop')
     end
 end
 
@@ -46,15 +71,30 @@ function unblock_sites()
     hs.alert.show("Websites Unblocked")
 end
 
-function log_event(event) 
-    local this_file , line
-    line = event
-    this_file = io.open(log_event_file, "a") 
-    this_file:write(line) 
+
+function encode_event(event)
+    local lines = {}
+    table.insert(lines, event.timestamp .. " " .. event.name)
+    for i, v in pairs(event.details) do
+        table.insert(lines, "\t:" .. i .. ":\t" .. v )
+    end
+    return table.concat(lines, "\n")
+end
+
+function flush_events() 
+    local this_file = io.open(log_event_file, "a") 
+    for i, event in ipairs(events_buffer) do 
+        this_file:write(encode_event(event) .. "\n")
+    end
     this_file:close() 
+    events_buffer = {}
+    events_buffer.length = 0
 end 
 
 prev_msg = ""
+prev_timestamp = os.date("%x %X", os.time())
+prev_time = os.time()
+prev_details = nil 
 function get_active_app()
     local script = [[
 tell application "System Events"
@@ -100,30 +140,43 @@ end if
 return {activeAppName, windowName, myUrl, myTitle}
     ]]
     if isPersonal then
-        print('Running personal laptop script')
+        -- print('Running personal laptop script')
         _, message,_ = hs.osascript.applescript(script_personal)
     else 
-        print('Running work laptop script')
+        -- print('Running work laptop script')
         _, message,_ = hs.osascript.applescript(script)
     end
     if message ~= nil then 
         msg_str = table.concat(message, "\n")
         if prev_msg ~= msg_str then 
-            log_event(os.date("%x %X", os.time()) .. " - " .. table.concat(message, ",") .. "\n")
+            if prev_details ~= nil then
+                log_event("application_changed", prev_details)
+            end
+            prev_details = { 
+                activeAppName = message[1],
+                windowName = message[2],
+                url = message[3],
+                title = message[4],
+                start = timestamp, 
+                durationSeconds = os.difftime(os.time(),prev_time)
+            }
             -- hs.alert.show(os.date("%x %X", os.time()) .. ":\n" .. table.concat(message, "\n"))
             prev_msg = msg_str
+            prev_timestamp = os.date("%x %X", os.time())
+            prev_time = os.time()
         end
     end
 end
 
-hs.timer.doEvery(5, get_active_app)
-    
+logger_timer =  hs.timer.new(1, get_active_app)
+logger_timer:start()
+get_active_app()
 
-hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'T', function() get_active_app();  end)
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'T', function() flush_events();  end)
 
 hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'B', function() block_sites() end)
 hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'U', function() unblock_sites() end)
 
 
-watcher = hs.caffeinate.watcher.new(f)
+watcher = hs.caffeinate.watcher.new(caffeineEvents)
 watcher:start()
