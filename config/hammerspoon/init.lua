2f69ec5c494ca99a890859b6653691f9e400217b
Bartosz <bartosz@warzecha.co.uk>
2021-01-21 21:34:54 +0000

    Restoring MiroWindowsManager with Hammerspoon. Couldln't find anything better

---
+++ config/hammerspoon/init.lua
@@ -0,0 +1,9 @@
+hs.window.animationDuration = 0
+
+local hyper = {"ctrl", "alt", "cmd"}
+hs.loadSpoon("MiroWindowsManager")
+spoon.MiroWindowsManager:bindHotkeys({ up = {hyper, "k"},
+  right = {hyper, "l"},
+  down = {hyper, "j"},
+  left = {hyper, "h"},
+  fullscreen = {hyper, "m"} })
