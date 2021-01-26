8c09a59cd3a8ae3146a6c021e131215ce6ba74d4
Bartosz <bartosz@warzecha.co.uk>
2021-01-26 15:39:40 +0000

    Nvim restore setup

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -5,5 +5,5 @@ hs.loadSpoon("MiroWindowsManager")
 spoon.MiroWindowsManager:bindHotkeys({ up = {hyper, "k"},
   right = {hyper, "l"},
   down = {hyper, "j"},
-  left = {hyper, "h"},
+  left = {hyper, "h"}, 
   fullscreen = {hyper, "m"} })
