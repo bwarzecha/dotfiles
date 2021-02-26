9d7d6e975a6859d1d90d213f9f309334b8762e27
Bartosz Warzecha <bartosz@warzecha.co.uk>
2021-02-26 09:58:00 +0000

    Blueutil - disable bluetooth on sleep to disconnect headphones

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -7,3 +7,27 @@ spoon.MiroWindowsManager:bindHotkeys({ up = {hyper, "k"},
   down = {hyper, "j"},
   left = {hyper, "h"}, 
   fullscreen = {hyper, "m"} })
+
+  -- turn off  bluetooth on sleen (lid closed)
+function bluetooth(power)
+    print("Setting bluetooth to " .. power)
+    local cmd = [[blueutil --power ]] .. power
+    print("Executing: " .. cmd)
+    hs.execute( cmd , true)
+end
+
+function caffeineEvents(event)
+    print('Received event' ..  event)
+    if event == hs.caffeinate.watcher.systemWillSleep then
+        bluetooth("off")
+    elseif event == hs.caffeinate.watcher.screensDidWake then
+        bluetooth("on")
+    elseif  event ==  hs.caffeinate.watcher.screensDidUnlock then
+    elseif  event ==  hs.caffeinate.watcher.screensDidLock then
+    elseif  event ==  hs.caffeinate.watcher.screensaverDidStart then
+    elseif  event ==  hs.caffeinate.watcher.screensaverDidStop then
+    end
+end
+
+watcher = hs.caffeinate.watcher.new(caffeineEvents)
+watcher:start()
